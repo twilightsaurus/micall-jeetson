685be8dea7521f46c700f93ca988772addc3e684
Manu <tuataur@googlemail.com>
2024-03-11 14:15:29 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+       <a href="https://x.com/MJeetson" className="x-link">
+        <img src="/x.png" alt="MJeeston on X" />
+       </a>
+        <a href="https://t.me/+oa-y9n41xa9iNmRk" className="tg-link">
+        <img src="/tg.png" alt="Moonjeeters TG group." />
+       </a>
+      </div>
+    </>
+  )
+}
+
+export default App
