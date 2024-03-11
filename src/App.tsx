18168c99f5031af397199c56f030276ba3ae6e7c
Manu <tuataur@googlemail.com>
2024-03-11 16:09:19 +0200

    add curve link

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ function App() {
        <a href="https://x.com/MJeetson" className="x-link">
         <img src="/x.png" alt="MJeeston on X"  className='x-icon' />
        </a>
+       <a href="https://www.pump.fun/J6VBL7mhqQTRYZEcY9eTuS5khKTZnXDXpr7GGvvk2Ea6" className="curve-link">
+        <img src="/curve.png" alt="Pump.fun curve"  className='curve-icon' />
+       </a>
         <a href="https://t.me/+oa-y9n41xa9iNmRk" className="tg-link">
         <img src="/tg.png" alt="Moonjeeters TG group." className='tg-icon' />
        </a>
